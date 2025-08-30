40700277edfaaa1bba75d196d64e059f2cba4cce
Your Name <you@example.com>
2025-08-30 00:26:33 -0400

    intento mostrar

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
-import java.util.hashMap;
+import java.util.HashMap;
 
 public class Main{
-    private HashMap<String, Beneficiario> beneficiarios;
+    private HashMap<String, Beneficiario> Beneficiario;
    
     
     public static void Main(String[] args) {
-       
+        
+        
+    }
+    public void mostrarMenu() {
+        System.out.println("1. Agregar Beneficiario");
+        System.out.println("2. Modificar Beneficiario");
+        System.out.println("3. Eliminar Beneficiario");
+        System.out.println("4. Listar Beneficiarios");
+        System.out.println("5. Salir");
     }
 
 
